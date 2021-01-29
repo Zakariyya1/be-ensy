f34cce14d0758bb871044bfd487fb7cec99b425d
Zakariyya <zakp100@gmail.com>
2021-01-29 21:37:58 +0000

    ready to deploy to Heroku

--- db/connection.js
+++ db/connection.js
@@ -1,5 +1,10 @@
+const ENV = process.env.NODE_ENV || 'development';
 const knex = require('knex');
-const dbConfig = require('../knexfile');
+
+const dbConfig =
+  ENV === 'production'
+    ? { connection: process.env.DATABASE_URL, client: 'pg' }
+    : require('../knexfile');
 
 const connection = knex(dbConfig);
 
