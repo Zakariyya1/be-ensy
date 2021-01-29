00129e6bd7a08b2b3efa1bed46adec4ad98ba3d9
Zakariyya <zakp100@gmail.com>
2021-01-29 22:03:19 +0000

    removes endpoints temporarily

--- routers/api-router.js
+++ routers/api-router.js
@@ -4,12 +4,10 @@ const apiRouter = express.Router();
 const topicsRouter = require('./topics-router');
 const usersRouter = require('./users-router');
 const commentsRouter = require('./comments-router');
-const endpoints = require('../endpoints.json');
 
 apiRouter.use('/topics', topicsRouter);
 apiRouter.use('/users', usersRouter);
 apiRouter.use('/articles', articlesRouter);
 apiRouter.use('/comments', commentsRouter);
-apiRouter.use('/', endpoints);
 
 module.exports = apiRouter;
