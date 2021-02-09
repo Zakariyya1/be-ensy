c2fb27f098e69caf829a2f3c2f41af4bbe0debcb
Zakariyya <zakp100@gmail.com>
2021-02-09 11:40:00 +0000

    enables CORS

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
 const {
   handleCustomErr,
   handlePSQLErr,
-  handleServerErr,
+  handleServerErr
 } = require('./controllers/errors-controller');
 const apiRouter = require('./routers/api-router');
+
+app.use(cors());
 
 app.use(express.json());
 
